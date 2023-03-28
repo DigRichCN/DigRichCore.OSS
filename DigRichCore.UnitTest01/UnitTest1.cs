bedb8f2c1e62792b192e46325625ac820403313f
zk <zhangke@dianbaobao.com>
2023-03-28 14:28:08 +0800

    缓存服务

--- DigRichCore.UnitTest01/UnitTest1.cs
+++ DigRichCore.UnitTest01/UnitTest1.cs
@@ -12,22 +12,6 @@ namespace DigRichCore.UnitTest01 {
         [TestMethod]
         public void TestMethod1() {
             IServiceCollection services = new ServiceCollection();
-            //services.AddTencentOSS((cfg) => {
-            //    cfg.AppId = "1251080462";
-            //    cfg.SecretId = "AKID0mIacO5MXT9yYtQy1P7cjlZBYgHdHZEc";
-            //    cfg.SecretKey = "GeliZEF4hDeplLE1tb6VeXBccWBbrscf";
-            //    cfg.Region = "ap-chengdu";
-            //    cfg.BucketName = "digrich-1251080462";
-            //    cfg.ImageServerDomain = "https://digrich-1251080462.cos.ap-chengdu.myqcloud.com";
-            //});
-            //services.AddAliyunOSS((x) => {
-            //    x.AccessKeyId = "LTAI4GGZJfUNeq1jg2ptRu9b";
-            //    x.AccessKeySecret = "3Z1LGYRjRk7rClDsya3mA8TxJ8wICA";
-            //    x.PrivateEndpoint = "oss-cn-shanghai.aliyuncs.com";
-            //    x.FileServerDomain = "live-tools.oss-cn-shanghai.aliyuncs.com";
-            //    x.ImageServerDomain = "live-tools.oss-cn-shanghai.aliyuncs.com";
-            //    x.BucketName = "live-tools";
-            //});
 
             services.AddLocalOSS(x => { x.ImageServerDomain = "baidu.com"; });
 
